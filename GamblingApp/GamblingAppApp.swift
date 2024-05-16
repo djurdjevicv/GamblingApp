64895cceb6d51ec07766498602be19b4730fdd3b
Vladimir Djurdjevic <vladimirdjurdjevic@pvh.com>
2024-05-16 20:52:50 +0200

    Initial Commit

---
+++ GamblingApp/GamblingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamblingAppApp.swift
+//  GamblingApp
+//
+//  Created by Vladimir Djurdjevic on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GamblingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
